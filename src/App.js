a1f03c1edb83c4ef5900cb84eb4cdd202dfa1f4d
eycho <chw3872@gmail.com>
2023-10-18 21:54:52 +0900

    make tag with for loop

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
 import './App.css';
 function Header(props) {
   return (
-  <header>
-    <h1><a href='/'>{props.title}</a></h1>
-  </header>
+    <header>
+      <h1><a href='/'>{props.title}</a></h1>
+    </header>
   );
 }
-function Nav() {
+function Nav(props) {
+  const lis = []
+  for(let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)   //자동으로 생성하는 경우 Unique한 key값을 부여해야함
+  }
   return (
     <nav>
-        <ol>
-          <li><a href='/read/1'>html</a></li>
-          <li><a href='/read/2'>css</a></li>
-          <li><a href='/read/3'>js</a></li>
-        </ol>
-      </nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
   );
 }
 
 function Article(props) {
   return (
     <article>
-        <h2>{props.title}</h2>
-        {props.body}
-      </article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
   );
 }
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'js', body: 'js is ...' }
+  ]
   return (
     <div>
       <Header title="WEB"></Header>
-      <Nav></Nav>
+      <Nav topics={topics}></Nav>
       <Article title="welocome" body="Hello, WEB"></Article>
     </div>
   );
