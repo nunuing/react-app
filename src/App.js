cb621768898e3de857820cbc443bcf6103edd950
eycho <chw3872@gmail.com>
2023-10-19 14:44:19 +0900

    add event parameter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 function Header(props) {
   return (
     <header>
-      <h1><a href='/'>{props.title}</a></h1>
+      <h1><a href='/' onClick={(event)=>{
+        event.preventDefault();       //reload 방지
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
     </header>
   );
 }
@@ -10,7 +13,12 @@ function Nav(props) {
   const lis = []
   for(let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)   //자동으로 생성하는 경우 Unique한 key값을 부여해야함
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);  //event.target : 이벤트를 유발시킨 태그
+      }}>{t.title}</a>
+      </li>)   //자동으로 생성하는 경우 Unique한 key값을 부여해야함
   }
   return (
     <nav>
@@ -37,8 +45,12 @@ function App() {
   ]
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="WEB" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title="welocome" body="Hello, WEB"></Article>
     </div>
   );
